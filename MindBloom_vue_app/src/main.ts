d267aa94b080b0db3a0599047ecfe932f97087b7
Blue <azuldebenedetti@gmail.com>
2025-09-28 11:20:01 +0100

    Create main.ts

---
+++ MindBloom_vue_app/src/main.ts
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+  faBook,
+  faMoneyBillWave,
+  faLocationDot,
+  faUsers,
+  faCartShopping,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBook, faMoneyBillWave, faLocationDot, faUsers, faCartShopping)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(router)
+
+app.mount('#app')
