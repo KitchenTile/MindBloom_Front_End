623dcd7f98d4f009b742a09335a261a935a35360
Blue <azuldebenedetti@gmail.com>
2025-09-28 11:20:44 +0100

    Create Header.vue

---
+++ MindBloom_vue_app/src/components/header/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import './Header.css'
+import Navigation from './Navigation.vue'
+</script>
+
+<template>
+  <div class="header">
+    <div class="image-container">
+      <a href="">
+        <img src="@/assets\MBSVGbeige.svg" alt="" class="logo" />
+      </a>
+    </div>
+    <h1 class="title">MIND BLOOM</h1>
+    <Navigation />
+  </div>
+</template>
