76a192cd5381266282bd732e926258a26e9fbf60
Blue <azuldebenedetti@gmail.com>
2025-09-30 11:04:55 +0100

    Update HomeView.vue

--- MindBloom_vue_app/src/views/HomeView.vue
+++ MindBloom_vue_app/src/views/HomeView.vue
@@ -1,37 +1,10 @@
 <script setup lang="ts">
 import Header from '../components/header/Header.vue'
-import LessonCard from '../components/LessonCard.vue'
+import LessonCard from '../components/cards/LessonCard.vue'
 import { getAllLessons } from '../api/fetchAPI'
 import { ref, onMounted } from 'vue'
 
-interface Lesson {
-  topic: string
-  price: number
-  location: string
-  numOfSpaces: number
-}
 const loading = ref(false)
-const cardInfo = ref<Lesson[]>([])
-const error = ref(null)
-
-async function fetchData() {
-  error.value = cardInfo.value = null
-  loading.value = true
-
-  try {
-    cardInfo.value = await getAllLessons()
-  } catch (err) {
-    error.value = err.toString()
-  } finally {
-    loading.value = false
-  }
-}
-
-onMounted(() => {
-  fetchData()
-})
-
-console.log(error)
 </script>
 
 <template>
@@ -39,14 +12,5 @@ console.log(error)
     <Header />
 
     <div v-if="loading" class="loading">Loading...</div>
-
-    <div v-for="(lesson, index) in cardInfo" :key="index">
-      <LessonCard
-        :topic="lesson.topic"
-        :price="lesson.price"
-        :location="lesson.location"
-        :numOfSpaces="lesson.numOfSpaces"
-      />
-    </div>
   </main>
 </template>
