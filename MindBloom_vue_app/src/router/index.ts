4c6c4844f2cfd4ba45cdc4f05e84daedf4ad5667
Blue <azuldebenedetti@gmail.com>
2025-10-03 17:54:39 +0100

    Create index.ts

---
+++ MindBloom_vue_app/src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import CartView from '../views/CartView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: '',
+      component: HomeView,
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView,
+    },
+    // {
+    //   path: '/lesson/:id',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: LessonPage,
+    //     children: [
+    //       { path: 'expand', component: EditLessonModal }
+    //     ]
+    // },
+  ],
+})
+
+export default router
