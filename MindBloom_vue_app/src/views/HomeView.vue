c3306f1eacb795586abd796d5e473e9f16929cdf
Blue <azuldebenedetti@gmail.com>
2025-10-10 12:46:02 +0100

    Update HomeView.vue

--- MindBloom_vue_app/src/views/HomeView.vue
+++ MindBloom_vue_app/src/views/HomeView.vue
@@ -3,14 +3,16 @@ import Header from '../components/header/Header.vue'
 import LessonCard from '../components/cards/LessonCard.vue'
 import { getAllLessons } from '../api/fetchAPI'
 import { ref, onMounted } from 'vue'
+import "./HomeView.css"
 
 const loading = ref(false)
 </script>
 
 <template>
   <main>
-    <Header />
-
     <div v-if="loading" class="loading">Loading...</div>
+    <div class="home-page-container">
+      
+    </div>
   </main>
 </template>
