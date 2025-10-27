18b704d1846140b4993ef03df8250fb22e303bc3
Blue <azuldebenedetti@gmail.com>
2025-10-27 19:18:51 +0000

    Update Navigation.vue

--- MindBloom_vue_app/src/components/header/Navigation.vue
+++ MindBloom_vue_app/src/components/header/Navigation.vue
@@ -1,7 +1,12 @@
-<script>
+<script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import Search from './Search.vue'
 import './Navigation.css'
+import { loginModalActice } from '../../store/store'
+import LogInModal from '../modals/LogInModal.vue'
+
+const openLoginModal = () => {
+  loginModalActice.value = true
+}
 </script>
 
 <template>
@@ -9,6 +14,9 @@ import './Navigation.css'
     <nav>
       <RouterLink to="/lessons" id="book"> <font-awesome-icon icon="book" /> </RouterLink>
       <RouterLink to="/cart" id="cart"> <font-awesome-icon icon="cart-shopping" /> </RouterLink>
+      <RouterLink to="/login" id="user" @click="openLoginModal">
+        <font-awesome-icon icon="user" />
+      </RouterLink>
     </nav>
   </div>
 </template>
