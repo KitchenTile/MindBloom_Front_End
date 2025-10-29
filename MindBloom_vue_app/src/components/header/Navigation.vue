d1dd0c237f9c1016e7dfdb26ee04ec11deea8f74
Blue <azuldebenedetti@gmail.com>
2025-10-29 17:55:32 +0000

    Update Navigation.vue

--- MindBloom_vue_app/src/components/header/Navigation.vue
+++ MindBloom_vue_app/src/components/header/Navigation.vue
@@ -1,20 +1,24 @@
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './Navigation.css'
-import { loginModalActice } from '../../store/store'
-import LogInModal from '../modals/LogInModal.vue'
+import { loginModalActive, order } from '../../store/store'
+import { computed } from 'vue'
 
 const openLoginModal = () => {
-  loginModalActice.value = true
+  loginModalActive.value = true
 }
+
+const isDisabled = computed(() => order.length === 0)
 </script>
 
 <template>
   <div class="nav-search-container">
     <nav>
       <RouterLink to="/lessons" id="book"> <font-awesome-icon icon="book" /> </RouterLink>
-      <RouterLink to="/cart" id="cart"> <font-awesome-icon icon="cart-shopping" /> </RouterLink>
-      <RouterLink to="/login" id="user" @click="openLoginModal">
+      <RouterLink to="/cart" id="cart" :disabled="isDisabled" :class="{ disabled: isDisabled }">
+        <font-awesome-icon icon="cart-shopping" />
+      </RouterLink>
+      <RouterLink to="" id="user" @click="openLoginModal">
         <font-awesome-icon icon="user" />
       </RouterLink>
     </nav>
