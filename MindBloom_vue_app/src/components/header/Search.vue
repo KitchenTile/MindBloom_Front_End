3e549469e7553774428bc85110e7c2dd31135014
Blue <azuldebenedetti@gmail.com>
2025-10-30 23:39:26 +0000

    Update Search.vue

--- MindBloom_vue_app/src/components/header/Search.vue
+++ MindBloom_vue_app/src/components/header/Search.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 import './Search.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import LessonCard from '../cards/LessonCard.vue'
+import { getAllLessons } from '../../api/fetchAPI'
 
 // const filterProps = defineProps({
 //   topic: String,
@@ -13,10 +15,32 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const searchTerm = ref('')
 const hasFocus = ref(false)
+const error = ref(null)
+const cardInfo = ref([])
 // const hidden = computed(() => hasFocus)
 const compact = computed(() => hasFocus)
 
 const emit = defineEmits(['addToOrder'])
+
+async function fetchData() {
+  error.value = null
+  cardInfo.value = []
+  console.log(cardInfo.value)
+
+  try {
+    const data = await getAllLessons()
+    cardInfo.value = [...data]
+  } catch (err) {
+    error.value = err.toString()
+  }
+  // } finally {
+  //   loading.value = false
+  // }
+}
+
+onMounted(async () => {
+  await fetchData()
+})
 </script>
 
 <template>
@@ -28,47 +52,21 @@ const emit = defineEmits(['addToOrder'])
         class="search-input"
         v-model="searchTerm"
       />
-      <!-- <svg
-        width="30"
-        height="30"
-        viewBox="0 0 33 32"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M19.0575 18.3905C19.5782 17.8698 20.4224 17.8698 20.9431 18.3905L28.2765 25.7239C28.7972 26.2446 28.7972 27.0888 28.2765 27.6095C27.7558 28.1302 26.9115 28.1302 26.3909 27.6095L19.0575 20.2761C18.5368 19.7554 18.5368 18.9112 19.0575 18.3905Z"
-          fill="#212529"
-        />
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M14.0003 6.66667C10.3184 6.66667 7.33366 9.65143 7.33366 13.3333C7.33366 17.0152 10.3184 20 14.0003 20C17.6822 20 20.667 17.0152 20.667 13.3333C20.667 9.65143 17.6822 6.66667 14.0003 6.66667ZM4.66699 13.3333C4.66699 8.17868 8.84567 4 14.0003 4C19.155 4 23.3337 8.17868 23.3337 13.3333C23.3337 18.488 19.155 22.6667 14.0003 22.6667C8.84567 22.6667 4.66699 18.488 4.66699 13.3333Z"
-          fill="#212529"
-        />
-      </svg> -->
-      <font-awesome-icon icon="magnifying-glass" />
+      <font-awesome-icon icon="magnifying-glass" class="search-icon" />
       <div class="search-results">
-        <!-- {searchTerm !== "" ? (
-            mergeAndSort.uniqueShows.length > 0 ? (
-              mergeAndSort.uniqueShows
-                .slice(0, 5)
-                .map((show) => (
-                  <SearchCard
-                    key={"search" + show.showId}
-                    show={show}
-                    showEvents={mergeAndSort.shows.filter(
-                      (showEvent) => showEvent.showId === show.showId
-                    )}
-                  />
-                ))
-            ) : (
-              <div class="not-found-container">
-                No results for {searchTerm}
-              </div>
-            )
-          ) : null} -->
+        <div v-for="(lesson, index) in cardInfo" :key="index" v-if="searchTerm">
+          <!-- <div v-if=""> -->
+
+          <LessonCard
+            :id="lesson._id"
+            :topic="lesson.topic"
+            :price="lesson.price"
+            :location="lesson.location"
+            :numOfSpaces="lesson.numOfSpaces"
+            :search="true"
+          />
+          <!-- </div> -->
+        </div>
       </div>
     </div>
   </div>
