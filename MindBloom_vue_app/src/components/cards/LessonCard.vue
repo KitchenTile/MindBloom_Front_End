5a64cbf7e018822411302e1c28e9e2bb608dd9b6
Blue <azuldebenedetti@gmail.com>
2025-10-30 23:39:05 +0000

    Update LessonCard.vue

--- MindBloom_vue_app/src/components/cards/LessonCard.vue
+++ MindBloom_vue_app/src/components/cards/LessonCard.vue
@@ -4,7 +4,7 @@ import './LessonCard.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { order, addToOrder, lessonModalActive } from '../../store/store'
 
-const cardProps = defineProps(['topic', 'price', 'location', 'numOfSpaces', 'id'])
+const cardProps = defineProps(['topic', 'price', 'location', 'numOfSpaces', 'id', 'search'])
 
 const amount = computed({
   get() {
@@ -44,37 +44,44 @@ const openLessonModal = () => {
 </script>
 
 <template>
-  <div class="card-container">
-    <div class="image-container" @click="openLessonModal">
-      <!-- <a href=""> -->
-      <svg width="100" height="100">
+  <div class="card-container" :class="{ search: search }">
+    <div class="image-container" @click="openLessonModal" :class="{ search: search }">
+      <font-awesome-icon icon="book-open" class="book-icon" />
+      <svg width="100" height="100" :class="{ search: search }">
         <circle cx="50" cy="50" r="50" fill="#1d5236" />
       </svg>
       <img src="@/assets\mindbloomsvgbeige.svg" alt="" class="image" />
-      <!-- </a> -->
     </div>
-    <div class="text-container">
+    <div class="text-container" :class="{ search: search }">
       <h1 class="title">{{ cardProps.topic }}</h1>
-      <div class="spaces-display-container">
-        <div class="icon-p-container" id="space-bar">
-          <!-- <font-awesome-icon icon="users" /> -->
-          <p id="spaces" class="description-text">Available spaces</p>
-          <p id="spaces" class="description-text" v-if="cardProps.numOfSpaces !== 0">
-            {{ cardProps.numOfSpaces }} / 10
+      <div :class="{ search: search }">
+        <div class="spaces-display-container" v-if="!search">
+          <div class="icon-p-container" id="space-bar">
+            <p id="spaces" class="description-text">Available spaces</p>
+            <p id="spaces" class="description-text" v-if="cardProps.numOfSpaces !== 0">
+              {{ cardProps.numOfSpaces }} / 10
+            </p>
+            <p id="full" class="description-text" v-else>full</p>
+          </div>
+          <div class="bar-container">
+            <div class="bar" :style="barStyle" />
+          </div>
+        </div>
+        <div class="icon-p-container" v-else>
+          <font-awesome-icon icon="users" />
+          <p class="description-text" v-if="cardProps.numOfSpaces !== 0">
+            {{ cardProps.numOfSpaces }}
           </p>
           <p id="full" class="description-text" v-else>full</p>
         </div>
-        <div class="bar-container">
-          <div class="bar" :style="barStyle" />
+        <div class="icon-p-container">
+          <font-awesome-icon icon="location-dot" />
+          <p id="location" class="description-text">{{ cardProps.location }}</p>
         </div>
-      </div>
-      <div class="icon-p-container">
-        <font-awesome-icon icon="location-dot" />
-        <p id="location" class="description-text">{{ cardProps.location }}</p>
-      </div>
-      <div class="icon-p-container">
-        <font-awesome-icon icon="money-bill-wave" />
-        <p id="price" class="description-text">{{ cardProps.price }}</p>
+        <div class="icon-p-container">
+          <font-awesome-icon icon="money-bill-wave" />
+          <p id="price" class="description-text">{{ cardProps.price }}</p>
+        </div>
       </div>
 
       <div class="buttons-container" :class="{ compact: hidden }">
