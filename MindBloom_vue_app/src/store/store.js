2eb3d4b97bf4eac097d5a29b4862a8ae1af91d07
Blue <azuldebenedetti@gmail.com>
2025-11-10 12:31:24 +0000

    Update store.js

--- MindBloom_vue_app/src/store/store.js
+++ MindBloom_vue_app/src/store/store.js
@@ -1,4 +1,5 @@
 import { ref, reactive } from 'vue'
+import { getAllLessons } from '../api/fetchAPI'
 
 // user object
 export const user = ref(null)
@@ -9,6 +10,9 @@ export const loginModalActive = ref(false)
 //lesson modal
 export const lessonModalActive = ref({ active: false, id: null })
 
+//checkout modal
+export const checkoutModalActive = ref(false)
+
 // order state and logic
 export const order = reactive([])
 
@@ -16,19 +20,33 @@ export const addToOrder = (newOrder) => {
   const newItem = ref(true)
   for (let i = 0; i < order.length; i++) {
     if (order[i].lessonId === newOrder.lessonId) {
-      order[i].numOfSpaces = newOrder.numOfSpaces
+      order[i].availability = newOrder.availability
       newItem.value = false
     }
-    if (order[i].lessonId === newOrder.lessonId && order[i].numOfSpaces === 0) {
+    if (order[i].lessonId === newOrder.lessonId && order[i].availability === 0) {
       order.splice(i, 1)
     }
   }
   if (newItem.value === true) {
     order.push(newOrder)
   }
-  console.log(order)
+  // console.log(order)
 }
 
 export function clearOrder() {
   order.length = 0
+}
+
+// fetch and store lessons
+export const cardInfo = ref([])
+
+export const fetchData = async () => {
+  cardInfo.value = []
+
+  try {
+    const data = await getAllLessons()
+    cardInfo.value = [...data]
+  } catch (err) {
+    console.log(err)
+  }
 }
