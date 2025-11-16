786cb0766cc1165d2b701659972c8eaed39766d0
Blue <azuldebenedetti@gmail.com>
2025-11-16 16:00:35 +0000

    Update HomeView.vue

--- MindBloom_vue_app/src/views/HomeView.vue
+++ MindBloom_vue_app/src/views/HomeView.vue
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { user } from '../store/store'
 import { checkSession, supabase, userSessionCheck } from '../utils/supabase'
 import BookUploadCompoenet from '../components/BookUploadCompoenet.vue'
+import router from '../router'
 
 const loading = ref(false)
 const prompt = ref('')
@@ -165,103 +166,7 @@ onMounted(async () => {
 <template>
   <main>
     <div class="home-page-container">
-      <div class="chats-chat-container">
-        <div class="all-chats-container">
-          <div class="title-button-container">
-            <h2 class="chats-title">Your Chats</h2>
-          </div>
-          <button class="add-chat-button" @click="addNewChat()">+ New Chat</button>
-          <div
-            class="chat"
-            v-for="(chat, index) in chats"
-            :key="chat.chat_id"
-            :class="{ selected: chatId === chat.chat_id }"
-          >
-            <div style="display: flex">
-              <div class="chat-tab" />
-              <button
-                v-if="chatIdToEdit !== chat.chat_id"
-                class="chat-button"
-                :for="chat"
-                :id="`chat-${index}`"
-                @click="[(chatId = chat.chat_id)]"
-              >
-                {{ chat.title }}
-              </button>
-              <input
-                v-else
-                class="title-edit"
-                :for="chat"
-                v-model="newTitle"
-                @keyup.enter="editTitleFunction(chat.chat_id, newTitle)"
-                @keyup.escape="cancelTitleEdit()"
-              />
-            </div>
-            <div style="display: flex; gap: 5px">
-              <font-awesome-icon
-                icon="pen-to-square"
-                class="icon"
-                @click="editTitle(chat)"
-                v-if="chatIdToEdit !== chat.chat_id"
-              />
-              <font-awesome-icon icon="xmark" class="icon" v-else @click="cancelTitleEdit()" />
-              <font-awesome-icon
-                v-if="chatIdToEdit !== chat.chat_id"
-                icon="trash"
-                class="icon"
-                @click="deleteChatFunction(chat.chat_id)"
-              />
-              <font-awesome-icon
-                icon="check"
-                class="icon"
-                @click="editTitleFunction(chat.chat_id, newTitle)"
-                v-else
-              />
-            </div>
-          </div>
-        </div>
-        <div class="chat-container">
-          <div ref="chatContainer" class="chat-content-container">
-            <div
-              v-for="(message, index) in chat"
-              class="chat-message-container"
-              :class="{ right: index % 2 == 0 }"
-            >
-              <p :for="message" :id="`m-${index}`" class="chat-message">{{ message }}</p>
-            </div>
-            <div v-if="loading" class="loading"></div>
-          </div>
-          <div class="chat-form-container fade-in fade-in-delay-3">
-            <div class="chat-form">
-              <input
-                type="text"
-                placeholder="ask me anything..."
-                name="chat-input"
-                v-model="prompt"
-                class="chat-input"
-                v-on:keyup.enter="handleSubmit"
-              />
-              <button class="chat-submit-button" aria-label="Start Chat" v-on:click="handleSubmit">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="22"
-                  height="22"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="chat-submit-icon"
-                >
-                  <path d="M5 12h14" />
-                  <path d="m12 5 7 7-7 7" />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div class="title">HOME PAGE</div>
     </div>
   </main>
 </template>
